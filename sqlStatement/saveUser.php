113bdcfd7df475565f857506ea9b1619c4a39131
KevinRudaO <ridakevin3@gmail.com>
2022-12-08 14:42:22 -0500

    sql

---
+++ sqlStatement/saveUser.php
@@ -0,0 +1,24 @@
+<?php
+$hostName = 'localhost';
+$database = 'so';
+$username ='root';
+$pass = '';
+
+// Create connection
+$conn = new mysqli($hostName,$username,$pass,$database);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO usuarios (name, userName, password)
+VALUES ('".$_POST["name"]."','".$_POST["userName"]."','".$_POST["password"]."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
